4db10e6801a032b87329ebd3775e8e31a69c672c
Joshua Jones <joshua.jones.software@gmail.com>
2024-02-25 16:32:34 -0500

    test sized request write

--- test/test_request.py
+++ test/test_request.py
@@ -5,7 +5,7 @@ from httpy import HttpRequest, SizedBodyReader, StreamBodyReader, read_request_f
 
 class TestPath(unittest.TestCase):
 
-    def test_request(self):
+    def test_request_streaming(self):
         body = (
             b"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et "
             b"dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip "
@@ -19,6 +19,28 @@ class TestPath(unittest.TestCase):
             header=[('Transfer-Encoding', 'chunked')],
             body=io.BufferedReader(io.BytesIO(body)),
             trailer=[('Signature', 'arolighroaeigfhjarlkseiklgfhaoli')]
+        )
+        buf = io.BytesIO()
+        writer = io.BufferedWriter(buf)
+        r.write_to(writer)
+        writer.flush()
+        buf.seek(0)
+        print(buf.read().decode('utf-8'))
+
+    def test_request_sized(self):
+        body = (
+            b"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et "
+            b"dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip "
+            b"ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore "
+            b"eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia "
+            b"deserunt mollit anim id est laborum.")
+
+        r = HttpRequest(
+            method='GET',
+            path='/hello',
+            header=[('Content-Length', f'{len(body)}')],
+            body=io.BufferedReader(io.BytesIO(body)),
+            trailer=[]
         )
         buf = io.BytesIO()
         writer = io.BufferedWriter(buf)
