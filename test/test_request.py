969effbc64ae3bebead01a248aec0029f8bdddbe
Joshua Jones <joshua.jones.software@gmail.com>
2024-03-02 10:39:46 -0500

    streaming socket

--- test/test_request.py
+++ test/test_request.py
@@ -37,7 +37,7 @@ class TestPath(unittest.TestCase):
         print(b)
         print(req.trailers)
 
-    def test_socket(self):
+    def test_sized_socket(self):
         body = (
             b"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et "
             b"dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip "
@@ -59,6 +59,48 @@ class TestPath(unittest.TestCase):
         request = client.send_request()
         request.add_header('Host', 'test.com')
         request.sized(5)
+        request.write_header('/hello')
+        request.write(b'hello')
+        request.close()
+
+        recv_request = server.receive_request()
+        body = recv_request.body.read_all()
+        print(body.decode())
+        recv_request.body.close()
+
+        recv_request = server.receive_request()
+        body = recv_request.body.read_all()
+        print(body.decode())
+        recv_request.body.close()
+
+        client.close()
+        server.close()
+
+        s_client.close()
+        s_server.close()
+
+    def test_stream_socket(self):
+        body = (
+            b"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et "
+            b"dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip "
+            b"ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore "
+            b"eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia "
+            b"deserunt mollit anim id est laborum.")
+
+        s_client, s_server = socket.socketpair()
+        client = HttpConnection(s_client)
+        server = HttpConnection(s_server)
+
+        request = client.send_request()
+        request.add_header('Host', 'test.com')
+        request.chunked()
+        request.write_header('/hello-world')
+        request.write(body)
+        request.close()
+
+        request = client.send_request()
+        request.add_header('Host', 'test.com')
+        request.chunked()
         request.write_header('/hello')
         request.write(b'hello')
         request.close()
